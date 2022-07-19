41c566f6ae18bf308c0c71eebe7f05a9ded36e7a
özcan çetin <cetin445@gmail.com>
2022-07-19 11:37:31 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import Review from './Review';
+import reviews from "./data"
+function App() {
+
+  return <main>
+    <div>
+      <h2 className='title'>Our Reviews</h2>
+    <div className='underline'></div>
+    </div>
+    {reviews.map((review)=>{
+      return(
+        <Review review={review}/>
+      )
+    })}
+    </main>
+  
+}
+
+export default App;
