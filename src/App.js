313294d853606236b9e65cc419b152cae7a225b2
özcan çetin <cetin445@gmail.com>
2022-07-19 13:10:51 +0300

    changePerson function was created

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React, { useState } from 'react';
-import Review from './Review';
-import reviews from "./data"
+import React, { useState } from "react";
+import Review from "./Review";
+// import reviews from "./data";
 function App() {
-
-  return <main>
-    <div>
-      <h2 className='title'>Our Reviews</h2>
-    <div className='underline'></div>
-    </div>
-    {reviews.map((review)=>{
-      return(
-        <Review review={review} key={review.id}/>
-      )
-    })}
+  return (
+    <main>
+      <div>
+        <h2 className="title">Our Reviews</h2>
+        <div className="underline"></div>
+      </div>
+      <Review />
     </main>
-  
+  );
 }
 
 export default App;
--- src/Review.js
+++ src/Review.js
@@ -2,8 +2,20 @@ import React, { useState } from "react";
 import people from "./data";
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
-const Review = ({ review }) => {
-  const { id, name, job, image, text } = review;
+const Review = () => {
+  const [review, setReview] = useState(people)
+  const [person, setPerson] = useState(0)
+
+  const changePerson = () => {
+    const number = Math.floor(Math.random() * (review.length-1));
+    if(person !== number){
+      setPerson(number)
+    }
+  }
+
+  // }
+    // console.log(review);
+  const { id, name, job, image, text } = review[`${person}`];
   return (
     <section className="container">
       <div className="review">
@@ -17,10 +29,10 @@ const Review = ({ review }) => {
         <div className="job">{job}</div>
         <div className="info">{text}</div>
         <div >
-          <FaChevronLeft className="prev-btn"/>
-          <FaChevronRight className="next-btn"/>
+          <FaChevronLeft className="prev-btn" onClick={()=>setPerson(person !== 0 ? person - 1 : review.length-1)}/>
+          <FaChevronRight className="next-btn" onClick={()=>setPerson(person !== review.length-1 ? person + 1 : 0)}/>
         </div>
-        <button className="random-btn">Surprise Me</button>
+        <button className="random-btn" onClick={()=>changePerson()}>Surprise Me</button>
       </div>
     </section>
   );
