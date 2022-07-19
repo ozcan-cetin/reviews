ad2c0db69f5d5807afbbdca0f52946f28bb4f5f9
özcan çetin <cetin445@gmail.com>
2022-07-19 12:20:49 +0300

    cards was completed

--- src/Review.js
+++ src/Review.js
@@ -6,12 +6,22 @@ const Review = ({ review }) => {
   const { id, name, job, image, text } = review;
   return (
     <section className="container">
-      <div className="img-container">
-        <img src={image} alt={name} className="person-img" />
+      <div className="review">
+        <div className="img-container">
+          <div className="quote-icon">
+            <FaQuoteRight />
+          </div>
+          <img src={image} alt={name} className="person-img" />
+        </div>
+        <div className="author"> {name}</div>
+        <div className="job">{job}</div>
+        <div className="info">{text}</div>
+        <div >
+          <FaChevronLeft className="prev-btn"/>
+          <FaChevronRight className="next-btn"/>
+        </div>
+        <button className="random-btn">Surprise Me</button>
       </div>
-      <div className="author"> {name}</div>
-      <div className="job">{job}</div>
-      <div className="info">{text}</div>
     </section>
   );
 };
